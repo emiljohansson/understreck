ee225cb4bbc76f85c07c40915cf7bbcd1a9ad411
Emil Johansson <emijohansson@deloitte.com>
2017-07-15 18:05:32 -0600

    minify

--- understreck.js
+++ understreck.js
@@ -1,5 +1,49 @@
+import chunk from './src/understreck.chunk'
+import every from './src/understreck.every'
+import filter from './src/understreck.filter'
+import find from './src/understreck.find'
+import findIndex from './src/understreck.findindex'
 import forEach from './src/understreck.foreach'
+import forEachRight from './src/understreck.foreachright'
+import get from './src/understreck.get'
+import includes from './src/understreck.includes'
+import indexOf from './src/understreck.indexof'
+import isArray from './src/understreck.isarray'
+import isBoolean from './src/understreck.isboolean'
+import isFunction from './src/understreck.isfunction'
+import isNull from './src/understreck.isnull'
+import isString from './src/understreck.isstring'
+import isUndefined from './src/understreck.isundefined'
+import keys from './src/understreck.keys'
 import map from './src/understreck.map'
+import noop from './src/understreck.noop'
+import pluck from './src/understreck.pluck'
+import reduce from './src/understreck.reduce'
+import some from './src/understreck.some'
+import toString from './src/understreck.tostring'
+import uniqueid from './src/understreck.uniqueid'
 
+exports.chunk = chunk
+exports.every = every
+exports.filter = filter
+exports.find = find
+exports.findIndex = findIndex
 exports.forEach = forEach
+exports.forEachRight = forEachRight
+exports.get = get
+exports.includes = includes
+exports.indexOf = indexOf
+exports.isArray = isArray
+exports.isBoolean = isBoolean
+exports.isFunction = isFunction
+exports.isNull = isNull
+exports.isString = isString
+exports.isUndefined = isUndefined
+exports.keys = keys
 exports.map = map
+exports.noop = noop
+exports.pluck = pluck
+exports.reduce = reduce
+exports.some = some
+exports.toString = toString
+exports.uniqueid = uniqueid
