4638cb2249dcf3382fa59802fca111746a7d497f
Emil Johansson <emijohansson@deloitte.com>
2017-07-16 13:44:50 -0600

    add isnumber

---
+++ src/understreck.isnumber/index.js
@@ -0,0 +1,3 @@
+import getTag from '../.internal/gettag'
+
+export default value => getTag(value) === '[object Number]'
---
+++ src/understreck.isnumber/test.js
@@ -0,0 +1,17 @@
+import test from 'ava'
+import isNumber from '../understreck.isnumber'
+
+test('false', t => {
+  t.false(isNumber())
+  t.false(isNumber(() => {}))
+  t.false(isNumber('4'))
+  t.false(isNumber(null))
+  t.false(isNumber(undefined))
+})
+
+test('true', t => {
+  t.true(isNumber(4))
+  t.true(isNumber(Number.MIN_VALUE))
+  t.true(isNumber(Infinity))
+  t.true(isNumber(NaN))
+})
