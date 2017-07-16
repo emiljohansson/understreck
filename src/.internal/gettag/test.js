508d30b052bcda61cdc1c708976305d30e6aac4a
Emil Johansson <emijohansson@deloitte.com>
2017-07-16 13:45:06 -0600

    add gettag

---
+++ src/.internal/gettag/test.js
@@ -0,0 +1,19 @@
+import test from 'ava'
+import getTag from '../understreck.tostring'
+
+const typeString = '[object String]'
+const typeFunction = '[object Function]'
+const typeNumber = '[object Number]'
+const typeBoolean = '[object Boolean]'
+
+test(t => {
+  t.is(getTag(new String()), typeString)  // eslint-disable-line no-new-wrappers
+  t.is(getTag(''), typeString)
+  t.is(getTag(new Function()), typeFunction)  // eslint-disable-line no-new-func
+  t.is(getTag(() => {}), typeFunction)
+  t.is(getTag(class {}), typeFunction)
+  t.is(getTag(new Number()), typeNumber)  // eslint-disable-line no-new-wrappers
+  t.is(getTag(0), typeNumber)
+  t.is(getTag(new Boolean()), typeBoolean)  // eslint-disable-line no-new-wrappers
+  t.is(getTag(false), typeBoolean)
+})
